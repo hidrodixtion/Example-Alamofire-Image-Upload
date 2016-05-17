b9773fa887155ed4f5ba6171ad05144c6eaaa36f
Adi Nugroho <hidrodixtion@gmail.com>
2016-05-18 06:21:47 +0700

    Initial Commit

---
+++ AlamoUpload/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlamoUpload
+//
+//  Created by Adi Nugroho on 5/18/16.
+//  Copyright © 2016 Adi Nugroho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
